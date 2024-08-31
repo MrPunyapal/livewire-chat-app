61a4c328c4cb9cf31d30b999ff85974462cec6e8
Punyapal Shah <mrpunyapal@gmail.com>
2024-08-31 20:37:53 +0530

    chore: Remove email verification functionality

--- routes/auth.php
+++ routes/auth.php
@@ -2,7 +2,6 @@
 
 declare(strict_types=1);
 
-use App\Http\Controllers\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
 use Livewire\Volt\Volt;
 
@@ -21,13 +20,6 @@ Route::middleware('guest')->group(function () {
 });
 
 Route::middleware('auth')->group(function () {
-    Volt::route('verify-email', 'pages.auth.verify-email')
-        ->name('verification.notice');
-
-    Route::get('verify-email/{id}/{hash}', VerifyEmailController::class)
-        ->middleware(['signed', 'throttle:6,1'])
-        ->name('verification.verify');
-
     Volt::route('confirm-password', 'pages.auth.confirm-password')
         ->name('password.confirm');
 });
