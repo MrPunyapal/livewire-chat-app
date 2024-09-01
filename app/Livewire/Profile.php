2c5d973f44009f597d94b770f1585507eb3cdf7c
Punyapal Shah <mrpunyapal@gmail.com>
2024-09-01 11:51:07 +0530

    Refactor Livewire components to use strict types and return View

--- app/Livewire/Profile.php
+++ app/Livewire/Profile.php
@@ -1,12 +1,15 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Livewire;
 
+use Illuminate\View\View;
 use Livewire\Component;
 
 class Profile extends Component
 {
-    public function render()
+    public function render(): View
     {
         return view('livewire.profile');
     }
