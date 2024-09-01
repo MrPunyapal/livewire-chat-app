9c260ac690377ad122b7625fac2d898cb318e50b
Punyapal Shah <mrpunyapal@gmail.com>
2024-09-01 14:57:32 +0530

    test: Add test for sending password reset link to invalid email

--- tests/Unit/Livewire/Pages/Auth/ForgotPasswordTest.php
+++ tests/Unit/Livewire/Pages/Auth/ForgotPasswordTest.php
@@ -17,3 +17,13 @@ test('reset password link can be requested', function () {
 
     Notification::assertSentTo($user, ResetPasswordNotification::class);
 });
+
+test('reset password link is not sent to invalid email', function () {
+    Notification::fake();
+
+    Livewire::test(ForgotPassword::class)
+        ->set('email', 'invalid-email@example.com')
+        ->call('sendPasswordResetLink');
+
+    Notification::assertNothingSent();
+});
