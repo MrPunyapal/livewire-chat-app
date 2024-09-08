07d7b97ea7bfcbc6288f9e816fa7f6be4c1552cd
Punyapal Shah <mrpunyapal@gmail.com>
2024-09-08 18:10:53 +0530

    Add chat navigation links to the layout

--- resources/views/livewire/layout/navigation.blade.php
+++ resources/views/livewire/layout/navigation.blade.php
@@ -15,6 +15,10 @@
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')" wire:navigate>
                         {{ __('Dashboard') }}
                     </x-nav-link>
+
+                    <x-nav-link :href="route('chats')" :active="request()->routeIs('chats')" wire:navigate>
+                        {{ __('Chats') }}
+                    </x-nav-link>
                 </div>
             </div>
 
@@ -65,6 +69,10 @@
         <div class="pt-2 pb-3 space-y-1">
             <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')" wire:navigate>
                 {{ __('Dashboard') }}
+            </x-responsive-nav-link>
+
+            <x-responsive-nav-link :href="route('chats')" :active="request()->routeIs('chats')" wire:navigate>
+                {{ __('Chats') }}
             </x-responsive-nav-link>
         </div>
 
