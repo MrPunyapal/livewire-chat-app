d214c9d9201662b7bfd002aa76c0b6da8a34ccf5
Punyapal Shah <mrpunyapal@gmail.com>
2026-03-04 19:15:58 +0530

    livewire v4 routes and component name changes

--- resources/views/livewire/pages/chats.blade.php
+++ resources/views/livewire/pages/chats.blade.php
@@ -1,6 +1,6 @@
 <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
     <div class="flex h-screen overflow-hidden">
-        <livewire:rooms.index />
-        <livewire:chats.index />
+        <livewire:rooms />
+        <livewire:chats />
     </div>
 </div>
