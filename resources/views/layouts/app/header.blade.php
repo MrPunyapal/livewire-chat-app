575b366efd851aabe851d4af556f548bc823335f
Punyapal Shah <mrpunyapal@gmail.com>
2026-05-08 22:51:06 +0530

    update repository links in header and sidebar to point to the new livewire-chat-app

--- resources/views/layouts/app/header.blade.php
+++ resources/views/layouts/app/header.blade.php
@@ -29,7 +29,7 @@
                     <flux:navbar.item
                         class="h-10 max-lg:hidden [&>div>svg]:size-5"
                         icon="folder-git-2"
-                        href="https://github.com/laravel/livewire-starter-kit"
+                        href="https://github.com/MrPunyapal/livewire-chat-app"
                         target="_blank"
                         :label="__('Repository')"
                     />
@@ -70,7 +70,7 @@
             <flux:spacer />
 
             <flux:sidebar.nav>
-                <flux:sidebar.item icon="folder-git-2" href="https://github.com/laravel/livewire-starter-kit" target="_blank">
+                <flux:sidebar.item icon="folder-git-2" href="https://github.com/MrPunyapal/livewire-chat-app" target="_blank">
                     {{ __('Repository') }}
                 </flux:sidebar.item>
                 <flux:sidebar.item icon="book-open-text" href="https://laravel.com/docs/starter-kits#livewire" target="_blank">
--- resources/views/layouts/app/sidebar.blade.php
+++ resources/views/layouts/app/sidebar.blade.php
@@ -23,7 +23,7 @@
             <flux:spacer />
 
             <flux:sidebar.nav>
-                <flux:sidebar.item icon="folder-git-2" href="https://github.com/laravel/livewire-starter-kit" target="_blank">
+                <flux:sidebar.item icon="folder-git-2" href="https://github.com/MrPunyapal/livewire-chat-app" target="_blank">
                     {{ __('Repository') }}
                 </flux:sidebar.item>
 
