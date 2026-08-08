cfb6f03a0c3ca7888d87101ae00037d40ce1879e
Punyapal Shah <mrpunyapal@gmail.com>
2026-08-08 19:47:49 +0530

    lint

--- tests/Feature/AddMembersTest.php
+++ tests/Feature/AddMembersTest.php
@@ -5,10 +5,7 @@ declare(strict_types=1);
 use App\Livewire\Rooms\AddMembers;
 use App\Models\Room;
 use App\Models\User;
-use Illuminate\Foundation\Testing\RefreshDatabase;
 use Livewire\Livewire;
-
-uses(RefreshDatabase::class);
 
 test('user can add new members to the room', function (): void {
 
